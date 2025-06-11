68893f8728b5e6e2bfb988236964314821f2003c
lionel <77909441+lionelgwk@users.noreply.github.com>
2025-06-11 18:19:08 +0800

    chore(ci): setup vitest + ci workflow (#4)
    * chore(vitest): added deps and scripts in respective package.json for vitest
    
    * chore(ci): removed placeholder script for test in packages/config
    
    * chore(vitest): added vitest types
    
    * chore(vitest): added vitest config with absolute path alias
    
    * chore(vitest): added vitest setup with mock routing and cleanup
    
    * chore(vitest): added vitest config for packages/ui
    
    * chore(vitest): added vitest setup with cleanup after each test
    
    * chore(ci): added configs for test to turbo.json
    
    * chore(ci): added placeholder test for button under packages/ui
    
    * chore(chromatic): modified to use action-setup@v4
    
    * chore(ci): vitest workflow with pnpm caching for performance
    
    * chore(ci): setup all workflows that use pnpm to make use of caching, and to use pnpm version specified in root package.json
    
    * chore(eslint): escape double quotes on storybook default page to get around eslint
    
    * chore(ci): added env for DATABASE_URL for web build
    
    * chore(ci): updated all workflow permissions or formatting

---
+++ packages/ui/vitest.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    include: ["**/*.test.{ts,tsx}"],
+    globals: true,
+    setupFiles: ["./vitest.setup.ts"],
+    coverage: {
+      reporter: ["text", "json", "html"],
+      exclude: ["**/node_modules/**", "**/*.d.ts"],
+    },
+    css: false,
+    passWithNoTests: true,
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+});

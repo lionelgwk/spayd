dcdefba10935d8143dde3bbe3c85d3f3707a59a6
lionel <77909441+lionelgwk@users.noreply.github.com>
2025-08-03 14:04:59 +0800

    refactor(backend): reuse of PrismaClient context and added test route (#6)
    * chore(storybook): default port switched to 6006 for dev mode
    
    * chore(cfg): added base node.json file in typescript-config
    
    * chore(backend): tsconfig extends from base node.json
    
    * v0feat(backend): initialise prisma and trpc plugins for fastify
    
    * v0feat(trpc): trpc context initialisation
    
    * chore(trpc): tsconfig extends from base node.json
    
    * chore(trpc): added createContext function used for each request
    
    * v0feat(backend): createApp to register plugins and start backend
    
    * v0feat(backend): start function to start backend and handle graceful shutdowns
    
    * v0feat(nodemon): nodemon setup for auto reload on watched files for better dev experience
    
    * v0feat(trpc): basic implementation of user router and index.ts to export all routers
    
    * chore(trpc): added package.json
    
    * chore(database): updated routes for main and types
    
    * chore(deps): updated dependencies with fastify and trpc setup
    
    * v0feat(workflow): scripts to setup dev workflow for better dev experience with single script/command
    
    * refactor(backend): usage of fastify's logger vs console.log
    
    * refactor(backend): refrain from creating a new PrismaClient every time
    
    * chore(backend): added route /hello for testing
    
    * chore(backend): added new test /hello route to router
    
    * chore(backend): init validations package.json

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -6,7 +6,7 @@ async function start() {
   try {
     const port = process.env.PORT ? parseInt(process.env.PORT) : 9000;
     await app.listen({ port, host: "0.0.0.0" });
-    console.log(`Server listening on port ${port}`);
+    app.log.info(`Server listening on port ${port}`);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
--- apps/backend/src/trpc/context.ts
+++ apps/backend/src/trpc/context.ts
@@ -1,9 +1,5 @@
 import { CreateFastifyContextOptions } from "@trpc/server/adapters/fastify";
-import { PrismaClient } from "@spayd/database";
 import type { Context } from "@spayd/trpc"; // we make use of the context type defined in @spayd/trpc
-
-// we create a single instance of Prisma Client
-const prisma = new PrismaClient();
 
 // this is the function that is used to create the context for the tRPC server for each request
 export async function createContext({
@@ -13,6 +9,7 @@ export async function createContext({
   return {
     req,
     res,
-    prisma,
+    prisma: req.server.prisma, // we make use of the prisma client that is already instantiated in the plugin
+    // no need to create a new instance every time
   };
 }
